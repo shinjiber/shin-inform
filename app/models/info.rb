cee4156cf14b767ab426e453a975173bdb7eb4d2
jienshinji.yamaguchi <shinjiber7@gmail.com>
2019-02-21 10:07:40 +0000

    toppage layout

--- app/models/info.rb
+++ app/models/info.rb
@@ -2,7 +2,7 @@ class Info < ApplicationRecord
   belongs_to :user
   
   validates :title, presence: true, length: {maximum: 255}
-  validates :content, presence: true, length: {maximum: 255}
+  validates :content, presence: true
   
   has_many :scores
 end
